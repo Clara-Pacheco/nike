d9c12f5a95592e04cd3da52b9cb8c3d57d77e8b0
Clara <clarapachecodev@gmail.com>
2023-10-06 20:59:06 -0300

    Build a navbar with logo, desktop menu and mobile

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,62 @@
+import { headerLogo } from '../assets/images'
+import { hamburger } from '../assets/icons'
+
 export const Nav = () => {
   return (
-    <div>
-      TESTE
-    </div>
+   <header className='padding-x py-8 absolute z-10 w-full'>
+    <nav className='flex justify-between items-center max-container'>
+      <a href="/">
+        <img
+          src={headerLogo}
+          alt="Nike check symbol logo"
+          width={130}
+          height={29}
+        />
+      </a>
+      <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
+        <li>
+          <a href="/"
+            className='font-montserrat leading-normal text-lg text-slate-gray'>
+              Home
+          </a>
+        </li>
+        <li>
+          <a href="/"
+            className='font-montserrat leading-normal text-lg text-slate-gray'>
+              About Us
+          </a>
+        </li>
+        <li>
+          <a href="/"
+            className='font-montserrat leading-normal text-lg text-slate-gray'>
+              Products
+          </a>
+        </li>
+        <li>
+          <a href="/"
+            className='font-montserrat leading-normal text-lg text-slate-gray'>
+            Contact Us
+          </a>
+        </li>
+        <li>
+          <a href="/"
+            className='font-montserrat leading-normal text-lg text-slate-gray'>
+            Sign in /Explore Now 
+          </a>
+        </li>
+      </ul>
+      <div className='hidden max-lg:block'>
+        <a href="/">
+          <img
+            src={hamburger}
+            alt="Nike check symbol logo"
+            width={25}
+            height={25}
+          />
+        </a>
+      </div>
+    </nav>
+   </header>
   )
 }
 
