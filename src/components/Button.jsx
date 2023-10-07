098d54f61cd6eaadf46e37583d33bb517f93b0d7
Clara <clarapachecodev@gmail.com>
2023-10-07 09:31:50 -0300

    Create Button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+export const Button = ({children}) => {
+  return (
+    <button className="text-white py-2 px-6 rounded  bg-[#FF6452] hover:bg-[#f24532] duration-500 max-lg:mt-1 max-lg:translate-x-5">
+
+      {children}
+
+    </button>
+  )
+}
+
