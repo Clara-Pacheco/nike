3299049499c09af0473b3a5ef78c66b6c6edf4c6
Clara <clarapachecodev@gmail.com>
2023-10-10 11:03:04 -0300

    Fix logic for burguer menu

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -22,27 +22,28 @@ export const Nav = () => {
     }
   ]
   const [open, setOpen] = useState(false)
- console.log(open)
+  console.log(open)
   return (
    <header className=' w-full fixed top-0 left-0'>
-    <nav className= 'flex max-lg:flex-col items-center justify-center gap-16 bg-white py-4 md:px-10 px-7'>
+    <nav className= 'max-lg:bg-slate-100 max-lg:shadow-lg max-xl:h-[600px] flex max-lg:flex-col justify-center gap-16 bg-white py-4  md:px-10 px-7'>
       <a href="/">
         <img 
-          className='max-lg:absolute max-lg:left-10 max-lg:top-6 lg:mt-2'
+          className=' absolute max-lg:left-8 lg:mt-5 max-lg:mt-[-260px] lg:ml-[-6.25rem]'
           src={headerLogo}
           alt="Nike check symbol logo"
           width={130}
           height={29}
         />
       </a>
-      <d
-      iv onClick={()=>setOpen(!open)} className='text-3xl absolute right-8 top-6 cursor-pointer lg:hidden'>
+
+      <div onClick={()=>setOpen(!open)} className='text-3xl absolute right-8 top-10 cursor-pointer lg:hidden'>
           <ion-icon name={open ? 'close' : 'menu'}></ion-icon>
-      </d>
-      <ul className={`absolute flex max-lg:flex-col justify-center items-center gap-16 ${open ? "top-20": "top-[-490px]"} max-lg:mt-3 lg:mt-3`}>
+      </div>
+
+      <ul className="absolute max-lg:hidden lg:flex w-full max-lg:flex-col justify-around gap-16 lg:mt-[6rem]">
         {Links.map((link)=>{
           return(
-            <li key={link.name} className='md:ml-8 text-xl md:my-0 ml-7'>
+            <li key={link.name} className='max-lg:m-5 max-lg:mb-10'>
               <a href={link.link}
                 className='font-montserrat leading-normal text-lg text-slate-gray hover:text-slate-400 duration-500'>
                   {link.name}
@@ -55,8 +56,31 @@ export const Nav = () => {
           SIGN IN/EXPLORE
         </Button>
       </ul> 
+      {open && (
+        <ul className="absolute max-lg:flex lg:hidden max-lg:flex-col justify-around gap-2 lg:mt-[6rem]">
+        {Links.map((link)=>{
+          return(
+            <li key={link.name} className='max-lg:m-5 max-lg:mb-10'>
+              <a href={link.link}
+                className='font-montserrat leading-normal text-lg text-slate-gray hover:text-slate-400 duration-500'>
+                  {link.name}
+              </a>
+            </li>
+           
+          )
+        })}
+        <Button >
+          SIGN IN/EXPLORE
+        </Button>
+      </ul> 
+    
+   )}
+      
     </nav>
+  
+
    </header>
+  
   )
 }
 
