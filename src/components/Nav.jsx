e77f132307e946d2618e89c9d26f37e9f8d5e68e
Clara <clarapachecodev@gmail.com>
2023-10-10 08:36:27 -0300

    Build hamburguer menu

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { headerLogo } from '../assets/images'
 import { Button } from '../components/Button'
 
@@ -20,18 +21,25 @@ export const Nav = () => {
      link: "/contactus"
     }
   ]
+  const [open, setOpen] = useState(false)
+ console.log(open)
   return (
-   <header className='shadow-md w-full fixed top-0 left-0'>
+   <header className=' w-full fixed top-0 left-0'>
     <nav className= 'flex max-lg:flex-col items-center justify-center gap-16 bg-white py-4 md:px-10 px-7'>
       <a href="/">
-        <img
+        <img 
+          className='max-lg:absolute max-lg:left-10 max-lg:top-6 lg:mt-2'
           src={headerLogo}
           alt="Nike check symbol logo"
           width={130}
           height={29}
         />
       </a>
-      <ul className='flex max-lg:flex-col justify-center items-center gap-16 max-lg:mt-3'>
+      <d
+      iv onClick={()=>setOpen(!open)} className='text-3xl absolute right-8 top-6 cursor-pointer lg:hidden'>
+          <ion-icon name={open ? 'close' : 'menu'}></ion-icon>
+      </d>
+      <ul className={`absolute flex max-lg:flex-col justify-center items-center gap-16 ${open ? "top-20": "top-[-490px]"} max-lg:mt-3 lg:mt-3`}>
         {Links.map((link)=>{
           return(
             <li key={link.name} className='md:ml-8 text-xl md:my-0 ml-7'>
